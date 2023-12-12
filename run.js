e566a85d02ae8f716467365df8013010da38cc18
John West <john.sheffield.west@gmail.com>
2023-12-12 09:41:07 -0500

    Differentiate between compilation and running a file
    Since we want to be able to run a file sometimes and other times
    save the intermediate compiled JavaScript, this enables that.

--- run.js
+++ run.js
@@ -1,12 +1,40 @@
 #!/usr/bin/env node
 
 import { readFileSync, writeFileSync } from "fs";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
 import { compile, lex } from "./compiler.js";
 
-const input = readFileSync(process.argv.slice(2)[0]).toString();
+const argv = yargs(hideBin(process.argv))
+	.usage("Usage: $0 <inputFile> [options]")
+	.command("$0 <inputFile> [outputFile]", "Run the program", (yargs) => {
+		yargs
+			.positional("inputFile", {
+				describe: "Path to input Ogipier file.",
+				type: "string",
+			})
+			.positional("outputFile", {
+				describe: "Path to the compiled JavaScript file",
+				type: "string",
+			});
+	})
+	.option("run", {
+		alias: "r",
+		describe: "Run the program immediately",
+		type: "boolean",
+		default: false,
+	})
+	.help().argv;
+
+const input = readFileSync(argv.inputFile).toString();
 
 const tokens = lex(input);
 
 const compiled = compile(tokens);
 
-eval(compiled);
+if (process.argv.includes("--run")) {
+	eval(compiled);
+} else {
+	writeFileSync(argv.outputFile, compiled);
+}
